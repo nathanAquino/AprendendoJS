4df50328d687c31cd8502cb2e035b178d54bf12c
nathanAquino <you@example.com>
2021-01-21 11:26:44 -0300

    Aula 3 -criando um cliente

--- API- Consumindo/api/cliente.js
+++ API- Consumindo/api/cliente.js
@@ -1,11 +1,31 @@
-listarClientes = () =>{
-    return fetch('http://localhost:4000/clientes')
-    
-    .then(resposta =>{
-        return resposta.json()
+const listarClientes = () => {
+    return fetch("http:localhost:4000/clientes", {
+      method: "get"
     })
-
-    .then(json =>{
-        return json
-    })
-}
+      .then(resp => {
+        return resp.json();
+      })
+      .then(json => {
+        return json;
+      });
+  };
+  
+  const cadastrarClientes = (nome, cpf) => {
+    const json = JSON.stringify({
+      nome: nome,
+      cpf: cpf
+    });
+  
+    return fetch("http:localhost:4000/clientes/cliente", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: json
+    }).then(resp => {
+      
+        return resp.body;
+      } 
+    );
+  }
+  
