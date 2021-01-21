a7ea31b1c03ba604cd41e413d870df81b4693093
nathanAquino <you@example.com>
2021-01-21 11:57:00 -0300

    Aula 4 -Deletando um cliente

--- API- Consumindo/listagem-cliente.js
+++ API- Consumindo/listagem-cliente.js
@@ -1,11 +1,17 @@
-const exibeCliente = (cpf, nome) => {
+const removeCliente = id => {
+  if(confirm("Deseja deletar o cliente ?"))
+  deletaCliente(id)
+}
+const exibeCliente = (cpf, nome, id) => {
   const linha = document.createElement("tr");
 
-  const conteudoLinha =`
-  
-  <td>${cpf}</td>
-  <td>${nome}</td>
-`;
+  const conteudoLinha = `
+      
+      <td>${cpf}</td>
+      <td>${nome}</td>
+      <button type="button" class="btn btn-danger"
+      onclick="removeCliente(${id})">Excluir</button>
+  `;
 
   linha.innerHTML = conteudoLinha;
 
@@ -16,6 +22,6 @@ const corpoTabela = document.querySelector("[data-conteudo-tabela]");
 
 listarClientes().then(clientes => {
   clientes.forEach(indice => {
-    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome));
+    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id));
   });
 });
